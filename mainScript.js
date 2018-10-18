de5fd0f26e464555b088cc0a0727bee8a27e50b8
Finn <cheekywombat420@gmail.com>
2018-10-18 17:00:59 +1100

    yeet

--- mainScript.js
+++ mainScript.js
@@ -82,7 +82,6 @@ var account = {red: 0, blue: 0, yellow: 0};
 var cash = 10000;
 var amount = 1;
 var currentTradePartner = "";
-console.log("CONNECTED");
 
 $(document).ready(function(){
 	$(".buy").hide();
