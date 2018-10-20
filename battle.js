85b0b9ce707920d9719d52ff1ef0764b80651893
Finn <cheekywombat420@gmail.com>
2018-10-20 23:44:06 +1100

    rarity added for enemies

--- battle.js
+++ battle.js
@@ -1,6 +1,6 @@
 var currentPlayerHP = playerMaxHealth;
 var enemyHP = 0;
-var enemy = "";
+var enemy = [];
 var currentPlayerStamina = playerMaxStamina;
 var playerRegenTimer = "";
 var checkStaminaTimer = "";
@@ -29,43 +29,74 @@ enemyGen = function(){
 	$("#enemyHealth").show();
 	$(".player").show();
 	if(randomNum > 50){
-		$("#Gnome").show();
-		// var audio = new Audio('audio/GnomedLong.mp3');
-		// audio.play();
-		enemy = "Gnome";
-		enemyHP = enemyListObj.Gnome.maxHP;
-		log(enemyListObj.Gnome.tagLine);
-		updateEnemy(enemyHP);
+		spawnEnemy("Gnome");
 	}
 	if(randomNum ==50){
 		spawnEnemy("Gnome");
 	}
 	if(randomNum < 50){
-		spawnEnemy("Highwayman");
-
-		enemyHP = enemyListObj.Highwayman.maxHP;
-		
-		updateEnemy(enemyHP);
+		spawnEnemy("Highwayman");	
 	}
-	var attackTimer = enemyListObj[enemy].attackInterval;
-	enemyAttackTimer = window.setInterval(enemyAttack, attackTimer);
-	var attackTimer = enemyListObj[enemy].attackInterval;
-	var enemyAttackAnimation = "#" + enemyListObj[enemy].animation;
+	
+}
+spawnEnemy = function(enemyName,){
+	var enemyRarityInfo = enemyRarity();
+	var rarity = enemyRarityInfo.rarity
+	var enemyMultiplier = enemyRarityInfo.multiplier;
+	console.log(rarity," ",enemyMultiplier,"-------")
+
+	enemyHP = enemyListObj[enemyName].maxHP;
+	var id = "#" + enemyName;
+	$(id).show();
+	enemy[0] = enemyName;
+	enemy[1] = rarity;
+	log(enemyListObj[enemyName].tagLine);
+	updateEnemy();
+	var attackTimer = enemyListObj[enemy[0]].attackInterval;
+	enemyAttackTimer = window.setInterval(enemyAttack, attackTimer,enemy,enemyMultiplier);
+	var attackTimer = enemyListObj[enemy[0]].attackInterval;
+	var enemyAttackAnimation = "#" + enemyListObj[enemy[0]].animation;
 	if(enemyAttackAnimation !== "#"){
 		console.log(enemyAttackAnimation,"is now playing-----");
 		$(enemyAttackAnimation).show();
 	}
 }
-spawnEnemy = function(enemyName,){
-	enemyHP = enemyListObj[enemyName].maxHP;
-	var id = "#" + enemyName;
-	$(id).show();
-	enemy = enemyName;
-	log(enemyListObj[enemyName].tagLine);
-	updateEnemy();
-}
 enemyRarity = function(){
-	var randomNum = Math.floor((Math.random() * 100) + 1);
+	var randomNum = Math.floor((Math.random() * 1000) + 1);
+	var enemyInfo = {
+		rarity:"common",
+		multiplier: 1,
+	}
+	if(randomNum >=500){
+		enemyInfo.rarity = "common";
+		enemyInfo.multiplier = 1;
+	}
+	if(randomNum < 500 && randomNum >= 200){
+		enemyInfo.rarity = "uncommon";
+		enemyInfo.multiplier = 1.5;
+	}
+	if(randomNum < 200 && randomNum >= 100){
+		enemyInfo.rarity = "Rare";
+		enemyInfo.multiplier = 3;
+	}
+	if(randomNum < 100 && randomNum >= 75){
+		enemyInfo.rarity = "ultra-rare";
+		enemyInfo.multiplier = 6;
+	}
+	if(randomNum < 75 && randomNum >= 65){
+		enemyInfo.rarity = "mythical";
+		enemyInfo.multiplier = 10;
+	}
+	if(randomNum < 65 && randomNum >= 60){
+		enemyInfo.rarity = "legendary";
+		enemyInfo.multiplier = 50;
+	}
+	if(randomNum < 60 && randomNum >= 1){
+		enemyInfo.rarity = "Medium Rare";
+		enemyInfo.multiplier = 2;
+	}
+	return enemyInfo;
+
 }
 attackEnemy = function(){
 	if(currentPlayerStamina < staminaUsePerAttack * staminaUsePerAttackMulti){
@@ -112,15 +143,15 @@ regenerateStamina = function(){
 
 
 
-enemyAttack = function(){
-	var enemyMaxDmg = enemyListObj[enemy].maxDmg;
-	var enemyMinDmg = enemyListObj[enemy].minDmg;
+enemyAttack = function(enemy,multi){
+	var enemyMaxDmg = enemyListObj[enemy[0]].maxDmg * multi;
+	var enemyMinDmg = enemyListObj[enemy[0]].minDmg * multi;
 	var enemyDamage = Math.floor((Math.random() * (enemyMaxDmg - enemyMinDmg) +enemyMinDmg));
 	currentPlayerHP -= enemyDamage;
-	log(enemy,"did ",enemyDamage ,"damage.");
+	log(enemy[0],"[",enemy[1],"]","did ",enemyDamage ,"damage.");
 	updatePlayer();
 	if(currentPlayerHP <= 0){
-		playerDead();
+		playerDead(enemy,multi);
 		return;
 	}
 
@@ -134,7 +165,7 @@ enemyDead = function(){
 	$(".enemy").hide();
 	$("#attackBtn").hide();
 	$("#travellingTitle").show();
-	var xpDrop = enemyListObj[enemy].xpDrop;
+	var xpDrop = enemyListObj[enemy[0]].xpDrop;
 	currentXp += xpDrop;
 	console.log("gained xp",xpDrop);
 	if(currentXp >= xpUntilLevel){
@@ -145,7 +176,7 @@ enemyDead = function(){
 	currentPlayerStamina = playerMaxStamina;
 	var cashGained = Math.floor((Math.random() * 100) + 1);
 	cash += cashGained;
-	log(enemy,"has been killed!","You got",cashGained,"credits and",xpDrop, "xp!",);
+	log(enemy[0],"[",enemy[1],"]","has been killed!","You got",cashGained,"credits and",xpDrop, "xp!",);
 	dayEnd();
 }
 playerDead = function(){
@@ -162,7 +193,7 @@ playerDead = function(){
 	$(".travelGridWrapper").show();
 	halfCash = cash * .5;
 	cash -= Math.floor(halfCash);
-	log("You died to", enemy,"and lost",halfCash,"cash");
+	log("You died to",enemy[0],"[",enemy[1],"]","and lost",halfCash,"cash");
 	currentPlayerHP = playerMaxHealth;
 	currentLocation.locationX = 0;
 	currentLocation.locationY = 0;
